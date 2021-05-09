d6518548653702b88c1dc4671330dcdad5286803
priyesh <priyesh@example.com>
2021-05-09 18:48:03 +0530

    modifying lambda code

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -2,7 +2,7 @@ import json
 
 def lambda_handler(event, context):
     # TODO implement
-    print('its working')
+    print('its working Jyotsna')
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
