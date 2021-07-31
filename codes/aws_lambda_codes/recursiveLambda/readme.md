ea921f6aa4439666679111ee94d1904bffc91a0c
priyesh <priyesh@example.com>
2021-07-31 15:50:53 +0530

    adding code for recursive lambda

--- codes/aws_lambda_codes/recursiveLambda/readme.md
+++ codes/aws_lambda_codes/recursiveLambda/readme.md
@@ -1,1 +1,15 @@
-### todo
+# Extract Data from database using recursive Lambda function
+
+## Introduction 
+This is one of the innovative thing whihc i developed recently at my work
+
+## Techinical Design
+
+## How did it make a difference to the business oor the organization
+
+
+
+
+they wanted to go with fargate approcah but data was less so i suggested to go with lmbda utilizing lambda's cncurreny  funtinality
+
+it saved business costs
--- codes/aws_lambda_codes/recursiveLambda/recursive_lambda.py
+++ codes/aws_lambda_codes/recursiveLambda/recursive_lambda.py
@@ -1,9 +1,24 @@
+"Extract data from Database"
+import os
 import json
+from utilities.utils import process_data, put_data_s3
+from utilities.processor import Processor
+#from notification library import notifunc
+s3_bucket = os.environ.get('targetS3Bucket')
+secret=os.environ.get('DBsecret')
+target_file_location=os.environ.get('TargetLoc') 
 
 def lambda_handler(event, context):
-    # TODO implement
-    print('its working Jyotsna')
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+  '''Main Lambda function used to open the config file, connect to heroku postgresql and get the data.
+    we can also enable service now notification
+  '''
+  try:
+    print(f'Lambda Context: {context}')
+    with open('config/table_name_list.json') as data:
+       jsondata = json.load(data)
+      
+    processor = Processor(event)
+    return processor.process(context, jsondata.get('Tables'), process_data,s3_bucket,target_file_location,put_data_s3,secret)
+  except Exception as e:
+    print(e)
+
